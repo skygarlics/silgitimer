e37d7d486fbb86375843752354289bf72bbe1c78
MANL <skygarlics@users.noreply.github.com>
2021-06-15 02:54:22 +0900

    Empty message in empty list

--- src/Components/CheckListViewer/index.tsx
+++ src/Components/CheckListViewer/index.tsx
@@ -1,21 +1,23 @@
 import React, { useContext } from 'react';
 import { CheckListContext } from "CheckListContext";
 import CheckItem from "./CheckItem";
-import { List } from '@material-ui/core';
+import { List, ListItem, Typography } from '@material-ui/core';
 
 interface Props {}
 
 const CheckListViewer = ({ }: Props) => {
     const { checkList } = useContext<ICheckListContext>(CheckListContext);
-
     return (
+        (checkList.length < 1) ?
+        <Typography variant="h4">List is Empty</Typography>
+        :
         <List>
-            {checkList.map((value) => {
-                let txt = value.value;
-                return (
-                    <CheckItem text={txt}/>
-                );
-            })}
+        {checkList.map((value) => {
+            let txt = value.value;
+            return (
+                <CheckItem text={txt}/>
+            );
+        })}
         </List>
     );
 };
